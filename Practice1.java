4b547871a44c3aad45b97098d371ed8e5fcb3d22
mizukiokumura <nihonnpart4@yahoo.co.jp>
2021-09-30 10:31:37 +0900

    練習問題1-1

---
+++ Practice1.java
@@ -0,0 +1,66 @@
+public class Practice1 {
+  public static void main(String[] args) {
+    // 練習問題1-1
+    System.out.println("Hello World");
+    // 練習問題1-2
+    int x = 11;
+    System.out.println("x=" + x);
+    // 練習問題1-3
+    int z = 13;
+    int y = 17;
+    System.out.println("z=" + z + ",y=" + y);
+    // 練習問題1-4
+    int m = 13 + 17;
+    System.out.println("m=" + m);
+    // 練習問題1-5
+    System.out.println(13 * 17);
+    // 練乳問題1-6
+    int g = 7;
+    int gg = g * 3; // x *= 3;
+    System.out.println(gg);
+    int gv = g / 2; // x /= 2;
+    System.out.println(gv);
+    // 練習問題1-7
+    int i = 5;
+    int u = 7;
+    int h = 0;
+    h = u;
+    u = i;
+    i = h;
+    System.out.println("i=" + i + ",u=" + u);
+    // 練習問題1-8
+    int k = 19;
+    int n = 23;
+    int f = 0;
+    f =  k * n;
+    System.out.println(f);
+    // 練習問題1-9
+    int t = 3;
+    int e = t * 2;
+    int r = t * 3;
+    int w = t * 4;
+    System.out.println(e); // t * 2
+    System.out.println(r); // x * 3
+    System.out.println(w); // x * 4
+    // 練習問題1-10
+    int s = 4;
+    int c = s ;
+    int b = s * s;
+    int d = s * s * s;
+    System.out.println(c); // x
+    System.out.println(b); // x * x
+    System.out.println(d); // x * x * x
+    // 練習問題1-11
+    int j = 4;
+    int a = j / 2;
+    int q = j % 2;
+    System.out.println(a); 
+    System.out.println(q);
+    // 練習問題1-12
+    int l = 1;
+    l++;
+    System.out.println(l);
+    l--;
+    System.out.println(l);
+  }
+}
