b7e5b5f8e793589a1d2d6151331865d6b94ce079
mizukiokumura <nihonnpart4@yahoo.co.jp>
2021-10-08 11:53:12 +0900

    練習問題繰り返し。

---
+++ Practice4.java
@@ -0,0 +1,40 @@
+import java.io.*;
+public class Practice4 {
+  public static void main(String[] args) throws IOException {
+    // 練習問題 4-1;
+    // for (int i = 0 ; i < 10; i++) {
+    //   System.out.println("SPAM" + i + "回目");
+    // }
+    // 練習問題 4-2;
+    // for (int i = 0; i < 9; i++) {
+    //   System.out.println(3 * (i + 1));
+    // }
+    // 練習問題 4-3;
+    // int x = 1;
+    // for(int n = 1; n <= 8; n++){
+    //   x *= 2;
+    //   System.out.println("2の" + n + "乗=" + x);
+    // }
+    // 練習問題 4-4;
+    // int u = 0;
+    // for(int i = 7; i > 0; i--) {
+    //   int n = i--;
+    //   u = i * n;
+    //   System.out.println( i + "回目の７の階乗は" + u);
+    // }
+    // int x = 1;
+    // for(int i = 2; i <= 7; i++) {
+    //   x *= i;
+    //   System.out.println(x);
+    // 練習問題 4-5;
+    // BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    // int x = 0;
+    // System.out.println("整数を入力してください");
+    // for(int i =0; i <10; i++) {
+    //   int u = Integer.parseInt(br.readLine());
+    //   x += u;
+    // }
+    // System.out.println("整数の平均値は" + (x / 10)  + "です");
+    
+  }
+}
