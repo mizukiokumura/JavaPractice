ef6233e99dae1f83f5b6cd0c902e6582ff13ed45
mizukiokumura <nihonnpart4@yahoo.co.jp>
2021-10-21 11:00:33 +0900

    練習問題7

--- Practice7/CoinCase.java
+++ Practice7/CoinCase.java
@@ -10,14 +10,64 @@ class CoinCase {
   // static int GetCount(int type, int num) {
 
   // }
+  // private int Yen500 = 0; コインの枚数を入れている。
+  // private int Yen100 = 0;
+  // private int Yen50 = 0;
+  // private int Yen10 = 0;
+  // private int Yen5 = 0;
+  // private int Yen1 = 0;
+  // public void AddCoins(int kind, int count) {
+  //   Switch(kind) { スイッチ文で書くんだなと思った。
+  //     case 500:
+  //       Yen500 += count; 
+  //       break;
+  //     case 100:
+  //       Yen100 += count;
+  //       break;
+  //     case 50:
+  //       Yen50 += count;
+  //       break;
+  //     case 10:
+  //       Yen10 += count;
+  //       break;
+  //     case 5:
+  //       Yen5 += count;
+  //       break;
+  //     case 1:
+  //       Yen1 += count;
+  //       break;
+  //   }
+  // }
+  // public int GetCount(int kind) {
+  //   switch(kind) { ここはよくわかんなかったけど、おそらく独立したつのメソッドとして、考えるとわかった、変数の中身が何枚あるかを返してるんだなと。
+  //     case 500:
+  //       return Yen500;
+  //     case 100:
+  //       return Yen100;
+  //     case 50:
+  //       return Yen50;
+  //     case 10:
+  //       return Yen10;
+  //     case 5:
+  //       return Yen5;
+  //     case 1:
+  //       return Yen1;
+  //   }
+  //   return 0;
+  // }
+  // public int GetAmount() {
+  //   return(Yen500 * 500) + (Yen * 100) + (Yen * 50) + (Yen10 * 10) + (Yen5 * 5) + Yen1; ここでなるほどなーと思った、そうだよなーそうすれば確かに計算できるなと思った。
+  // }
+  // 練習問題 7-6
   private int Yen500 = 0;
   private int Yen100 = 0;
   private int Yen50 = 0;
   private int Yen10 = 0;
   private int Yen5 = 0;
   private int Yen1 = 0;
-  public void AddCoins(int kind, int count) {
-    Switch(kind) {
+  public void GetCoins(int kind, int count) {
+    
+    switch(kind) {
       case 500:
         Yen500 += count;
         break;
@@ -38,6 +88,7 @@ class CoinCase {
         break;
     }
   }
+  
   public int GetCount(int kind) {
     switch(kind) {
       case 500:
@@ -55,7 +106,36 @@ class CoinCase {
     }
     return 0;
   }
+  public void GetCount() {
+    int total = Yen500 + Yen100 + Yen50 + Yen10 + Yen5 + Yen1;
+    System.out.println(total + "枚です");
+
+  }
+  public void GetAmount(int kind) {
+
+    switch(kind) {
+      case 500:
+        System.out.println("500円の総額は" + (Yen500 * 500) + "円です");
+        break;
+      case 100:
+        System.out.println("100円の総額は" + (Yen100 * 100) + "円です");
+        break;
+      case 50:
+        System.out.println("50円の総額は" + (Yen50 * 50) + "円です");
+        break;
+      case 10:
+        System.out.println("10円の総額は" + (Yen10 * 10) + "円です");
+        break;
+      case 5:
+        System.out.println("5円の総額は" + (Yen5 * 5) + "円です");
+        break;
+      case 1:
+        System.out.println("1円の総額は" + (Yen1 * 1) + "円です");
+        break;
+
+    }
+  } 
   public int GetAmount() {
-    return(Yen500 * 500) + (Yen * 100) + (Yen * 50) + (Yen10 * 10) + (Yen5 * 5) + Yen1;
+    return (Yen500 * 500) + (Yen100 * 100) + (Yen50 * 50) + (Yen10 * 10) + (Yen5 * 5) + Yen1;
   }
 }
--- Practice7/Main.java
+++ Practice7/Main.java
@@ -33,28 +33,52 @@ public class Main {
     // dg.SetName(name);
     // dg.SetAge(age);
     // dg.ShowProfile();
+    // 練習問題 7-5;
+    //  BufferedReader br = new BufferedReader(
+    //                             new InputStreamReader( System.in ) );
+    //     CoinCase MyCase = new CoinCase();
+
+    //     for( int i = 0 ; i < 10 ; i++ )
+    //     {
+    //         System.out.print( "硬貨の種類は?" );
+    //         int kind = Integer.parseInt( br.readLine() );
+    //         System.out.print( "硬貨の枚数は?" );
+    //         int count = Integer.parseInt( br.readLine() );
+
+    //         MyCase.AddCoins( kind, count );
+    //     }
+
+    //     System.out.println( "500円は" + MyCase.GetCount( 500 ) + "枚" );
+    //     System.out.println( "100円は" + MyCase.GetCount( 100 ) + "枚" );
+    //     System.out.println( "50円は" + MyCase.GetCount( 50 ) + "枚" );
+    //     System.out.println( "10円は" + MyCase.GetCount( 10 ) + "枚" );
+    //     System.out.println( "5円は" + MyCase.GetCount( 5 ) + "枚" );
+    //     System.out.println( "1円は" + MyCase.GetCount( 1 ) + "枚" );
+    //     System.out.println( "総額は" + MyCase.GetAmount() + "円" );
+    // }
     // 練習問題 7-6;
-     BufferedReader br = new BufferedReader(
-                                new InputStreamReader( System.in ) );
-        CoinCase MyCase = new CoinCase();
-
-        for( int i = 0 ; i < 10 ; i++ )
-        {
-            System.out.print( "硬貨の種類は?" );
-            int kind = Integer.parseInt( br.readLine() );
-            System.out.print( "硬貨の枚数は?" );
-            int count = Integer.parseInt( br.readLine() );
-
-            MyCase.AddCoins( kind, count );
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      CoinCase Coin = new CoinCase();
+      int CheckCoin = 0;
+      for(int i = 0; i < 10; i++) {
+        System.out.print("硬貨の種類は?(500,100,50,10,5,1)");
+        int kind = Integer.parseInt(br.readLine());
+        System.out.print("硬貨の枚数は?");
+        int count = Integer.parseInt(br.readLine());
+        Coin.GetCoins(kind, count);
+      }
+      System.out.println("どの硬貨の事を知りたいですか?終了したい場合0を入力してください");
+      while(true) {
+        System.out.println("どの硬貨の事を知りたいですか?(終了したい場合0を入力してください)");
+        CheckCoin = Integer.parseInt(br.readLine());
+        if(CheckCoin == 0) {
+          break;
+        } else {
+          Coin.GetAmount(CheckCoin);
         }
+      }
+      Coin.GetCount();
+      System.out.println(Coin.GetAmount());
 
-        System.out.println( "500円は" + MyCase.GetCount( 500 ) + "枚" );
-        System.out.println( "100円は" + MyCase.GetCount( 100 ) + "枚" );
-        System.out.println( "50円は" + MyCase.GetCount( 50 ) + "枚" );
-        System.out.println( "10円は" + MyCase.GetCount( 10 ) + "枚" );
-        System.out.println( "5円は" + MyCase.GetCount( 5 ) + "枚" );
-        System.out.println( "1円は" + MyCase.GetCount( 1 ) + "枚" );
-        System.out.println( "総額は" + MyCase.GetAmount() + "円" );
-    }
   }
 }
