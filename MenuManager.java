4041e8b89e78382671f43e9d18c9f40190424b84
mizukiokumura <nihonnpart4@yahoo.co.jp>
2021-10-26 11:11:53 +0900

    練習問題

---
+++ MenuManager.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+public classMenuManager {
+  Map<String,ArrayList<String>> map = new HashMap<>();
+  MenuManager() {
+    ArrayList<String> list = new ArrayList<>();
+    list.add("ハンバーガー");
+    list.add("チーズバーガー");
+    list.add("ダブルチーズバーガー");
+    map.put("sandwitch", list);
+    list = new ArrayList<>();
+    list.add("コーラ");
+    list.add("オレンジジュース");
+    list.add("ミネラルウォーター");
+    map.put("drink",list);
+    list = new ArrayList<>();
+    list.add("フライドポテト");
+    list.add("チキンナゲット");
+    list.add("フレッシュサラダ");
+    map.put("sidmenu",list);
+
+    void searchMenu(String menu) {
+      for(String key: map.keySet()) {
+        if(map.get(key).contains(menu)) {
+          System.out.println(menu + "は" + key + "に含まれています。");
+          return;
+        }
+      }
+      System.out.println("該当するメニューはありません");
+    }
+    void setMenu(String category,String menu) {
+      map.get(category).add(menu);
+    }
+    void showMenu(String category) {
+    System.out.println(map.get(category));
+    }
+  }
+}
---
+++ Practice8.java
@@ -0,0 +1,37 @@
+import java.util.*;
+public class Practice8 {
+  public static void main(String[] args) {
+    // ArrayList<String> fruits = new ArrayList<>();
+    // fruits.add("りんご");
+    // fruits.add("みかん");
+    // fruits.add("バナナ");
+    // fruits.add("ブドウ");
+    // for(String fruit : fruits ) {
+    //   System.out.println(fruit); }
+
+    // Set<String> set = new HashSet<>();
+    // set.add("りんご");
+    // set.add("みかん");
+    // set.add("バナナ");
+    // set.add("ブドウ");
+    // set.remove("みかん");
+    // System.out.println(set);
+    // System.out.println(set.contains("バナナ"));
+    // System.out.println(set.contains("みかん"));
+    
+    // HashMap<String,Integer> tenSuu = new HashMap<>();
+    // tenSuu.put("国語:", 90);
+    // tenSuu.put("数学:", 80);
+    // tenSuu.put("英語:", 70);
+    // for(String tokuten: tenSuu.keySet()) {
+    //   System.out.println(tokuten + tenSuu.get(tokuten));
+    // }
+
+    Map<String, ArrayList<String>> map = new HashMap<>();
+    
+    ArrayList<String> sands = new ArrayList<>();
+    ArrayList<String> drinks = new ArrayList<>();
+    ArrayList<String> sides = new ArrayList<>();
+
+  }
+}
