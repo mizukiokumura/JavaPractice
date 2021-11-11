794b214c4acf5396697f27b8a428466628eecc0b
mizukiokumura <nihonnpart4@yahoo.co.jp>
2021-11-11 10:50:29 +0900

    練習問題

--- Practice27.java
+++ Practice27.java
@@ -71,22 +71,108 @@ public class Practice27 {
 
     // 問題1-5;
 
-    int age = -2;
+    // int age = -2;
 
-    System.out.println("年齢:" + age);
+    // System.out.println("年齢:" + age);
 
-    if(age < 0) {
-      System.out.println("エラー");
-    } else if (age < 20  ) {
-      System.out.println("未成年です");
-      if(age < 6 || age <= 14) {
-        System.out.println("今は義務教育を受けています");
-      } else {
-        System.out.println("今は義務教育を受けていません");
-      }
-    } else {
-      System.out.println("成人です");
-    }
+    // if(age < 0) {
+    //   System.out.println("エラー");
+    // } else if (age < 20  ) {
+    //   System.out.println("未成年です");
+    //   if(age < 6 || age <= 14) {
+    //     System.out.println("今は義務教育を受けています");
+    //   } else {
+    //     System.out.println("今は義務教育を受けていません");
+    //   }
+    // } else {
+    //   System.out.println("成人です");
+    // }
+
+    // 問題1-6;
+    // double height = 170.0;
+    // double weight = 55.0;
+    // double mHeight = height / 100;
+    // double bmi = weight / (mHeight * mHeight) ;
+
+    //  System.out.println("身長：" + height + "（cm）");
+    //  System.out.println("体重：" + weight + "（kg）");
+
+    // System.out.println("あなたのBMI値は" + bmi + "です");
+    // if(bmi >= 26.5) {
+    //   System.out.println("肥満です");
+    // } else if(bmi >= 24 && bmi < 25.5) {
+    //   System.out.println("太り気味です");
+    // } else if(bmi < 24 && bmi >= 20) {
+    //   System.out.println("普通です");
+    // } else if(bmi < 20 && bmi >= 0) {
+    //   System.out.println("スリムです");
+    // } else {
+    //   System.out.println("エラーです");
+    // }
+
+    // 問題1-7;
+
+    // int month = 13;
+
+    // System.out.println(month + "月");
+
+    // switch(month) {
+    //   case 1,3,5,7,8,10,12:
+    //     System.out.println(month + "月は31日まであります");
+    //     break;
+    //   case 4,6,9,11:
+    //     System.out.println(month + "月は30日まであります");
+    //     break;
+    //   case 2:
+    //     System.out.println(month + "月は28、または、29日まであります");
+    //     break;
+    //   default :
+    //     System.out.println("エラー");
+    //     break;
+        
+    // }
+
+    // 問題 1-8;
+    // for(int i = 21; i < 29; i++) {
+    //   if(i % 3 == 0) {
+    //     System.out.println(i + "は3の倍数です");
+    //   }
+    //   if(i % 4 == 0) {
+    //     System.out.println(i + "は4の倍数です");
+    //   }
+    //   if(i % 5 == 0 ) {
+    //     System.out.println(i + "は5の倍数です");
+    //   }
+    //   if(i % 6 == 0) {
+    //     System.out.println(i + "は6の倍数です");
+    //   }
+    //   if(i % 7 == 0) {
+    //     System.out.println(i + "は7の倍数です");
+    //   }
+    //   if(i % 8 == 0) {
+    //     System.out.println(i + "は8の倍数です");
+    //   }
+    //   if(i % 9 == 0) {
+    //     System.out.println(i + "は9の倍数です");
+    //   }
+    // }
+
+    // 問題1-9;
+    //   int i,j,yakusuu;
+    // for(i = 1; i < 100; i++) {
+    //   yakusuu = 0;
+    //   for(j = 1; j<= i; j++) {
+    //     if(i % j == 0) {
+    //       yakusuu++;
+    //     }
+    //   }
+    //     if(yakusuu == 2) {
+    //       System.out.println( i);
+    //     }
+      
+    // }
+    // System.out.printf("\n");
+
 
   }
 }
