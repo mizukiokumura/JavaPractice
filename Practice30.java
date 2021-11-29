71e987992c3e0986232bd07d1c2cd9d9da3919c0
mizukiokumura <nihonnpart4@yahoo.co.jp>
2021-11-29 11:09:58 +0900

    プログラム作成問題

---
+++ Practice30.java
@@ -0,0 +1,94 @@
+import java.util.ArrayList;
+public class Practice30 {
+  public static void main(String[] args) {
+    // プログラム作成問題 44
+
+    // int num = 123;
+    // String numS = Integer.toString(num);
+
+    // System.out.println(numS);
+
+    // 回答例
+    // int n = 123;
+    // String  str = "" + n;
+
+    // System.out.println(str);
+
+    // プログラム作成問題 45
+
+    // double[] dArray = {1.1 , 2.2, 3.3, 4.4, 5.5, 6.6, 7.7};
+
+    // int[] iArray = new int[dArray.length];
+    // for(int i = 0; i < dArray.length; i++) {
+    //   iArray[i] = (int)dArray[i];
+    // }
+
+    // for(int n = 0; n < iArray.length; n++) {
+    //   System.out.println(iArray[n]);
+    // }
+
+    // プログラム作成問題 46
+    // ArrayList<Double> dArray = new ArrayList<>();
+    // ArrayList<Integer> iArray = new ArrayList<>();
+    // dArray.add(1.1);
+    // dArray.add(2.2);
+    // dArray.add(3.3);
+    // dArray.add(4.4);
+    // dArray.add(5.5);
+
+    // for(int i = 0; i < dArray.size(); i++) {
+    //   iArray.add((int)Math.round(dArray.get(i)));
+    // }
+
+    // for(int iary : iArray) {
+    //   System.out.println(iary);
+    // }
+
+    // 回答例
+
+    // ArrayList<Double> doubleValues = new ArrayList<>();
+    // doubleValues.add(1.1);
+    // doubleValues.add(2.2);
+    // doubleValues.add(3.3);
+
+    // ArrayList<Integer> intValues = new ArrayList<>();
+
+    // for(double val : doubleValues) {
+    //   intValues.add((int)val);
+    // }
+
+    // for(int val : intValues) {
+    //   System.out.println(val);
+    // }
+
+    // プログラム作成問題 47
+
+    // String ten = "10";
+    // String niZyu = "20";
+
+    // int t = Integer.parseInt(ten);
+    // int n = Integer.parseInt(niZyu);
+
+    // System.out.println(t + n);
+
+    // プログラム作成問題 48
+
+    // int[] nums = { 4, 5, 8, 9, 10, 2, 5, 6};
+    // int total = 0;
+    // double avg = 0.0;
+
+    // for(int i = 0; i < nums.length; i++) {
+    //   total += nums[i];
+    // }
+    // avg = (float)total / nums.length;
+
+    // System.out.println("合計" + total);
+    // System.out.println("平均" + avg);
+
+
+
+
+
+
+  }
+}
